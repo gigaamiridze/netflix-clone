1f55687ec2159b3d7f1ddaa608852dc2261d5ce0
justtfelix <justttfelix@gmail.com>
2023-06-24 13:54:54 +0400

    [Client] Added 'useScrollPosition' custom hook

---
+++ client/src/hooks/useScrollPosition.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+function useScrollPosition() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const updateScroll = () => {
+      if (window.scrollY >= 30) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', updateScroll);
+
+    return () => window.removeEventListener('scroll', updateScroll);
+  }, []);
+
+  return isScrolled;
+}
+
+export default useScrollPosition;
