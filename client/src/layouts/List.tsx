6bef8efaf29862d78b2ae9cbae9f6752be69a350
justtfelix <justttfelix@gmail.com>
2023-06-26 11:30:50 +0400

    [Client] Fixed slider error

--- client/src/layouts/List.tsx
+++ client/src/layouts/List.tsx
@@ -1,20 +1,24 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import { ListContainer, SliderWrapper, Items } from '../components';
 import { ListItem } from '.';
 
 function List() {
+  const [slideNumber, setSlideNumber] = useState<number>(0);
   const listRef = useRef<HTMLDivElement>(null);
-  const items = Array(20).fill(0);
+  const items = Array(10).fill(0);
 
   const handleClick = (direction: string) => {
     if (listRef.current) {
       const distance = listRef.current.getBoundingClientRect().x - 50;
 
-      if (direction === 'left') {
+      if (direction === 'left' && slideNumber > 0) {
         listRef.current.style.transform = `translateX(${240 + distance}px)`;
-      } else {
+        setSlideNumber(slideNumber - 1);
+      }
+      if (direction === 'right' && slideNumber < 5) {
         listRef.current.style.transform = `translateX(${-240 + distance}px)`;
+        setSlideNumber(slideNumber + 1);
       }
     }
   }
