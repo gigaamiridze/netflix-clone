ef184933c7559a45f501f8b74a69ffde15d5fe5f
justtfelix <justttfelix@gmail.com>
2023-06-28 09:45:14 +0400

    [Client] Added video

--- client/src/components/listItem/ItemContainer.ts
+++ client/src/components/listItem/ItemContainer.ts
@@ -16,11 +16,20 @@ const ItemContainer = styled.div`
     object-fit: cover;
   }
 
+  video {
+    width: 100%;
+    height: 140px;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
   &:hover {
     width: 325px;
     height: 300px;
     position: absolute;
-    top: -150px;
+    top: -40px;
     -webkit-box-shadow: 0px 0px 15px 0px rgba(255, 255, 255, 0.07);
     box-shadow: 0px 0px 15px 0px rgba(255, 255, 255, 0.07);
     border-radius: 6px;
--- client/src/layouts/ListItem.tsx
+++ client/src/layouts/ListItem.tsx
@@ -8,6 +8,7 @@ import { ItemContainer, ItemInfo, Icons, Details } from '../components';
 function ListItem({ index }: IListItemProps) {
   const [isHovered, setIsHovered] = useState<boolean>(true);
   const distance = index * 225 - 50 + index * 2.5;
+  const trailer = 'https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761';
 
   return (
     <ItemContainer
@@ -17,24 +18,27 @@ function ListItem({ index }: IListItemProps) {
     >
       <img src='https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee' alt='Movie' />
       {isHovered && (
-        <ItemInfo>
-          <Icons>
-            <BsPlayFill />
-            <MdOutlineAdd />
-            <AiOutlineLike />
-            <AiOutlineDislike />
-          </Icons>
-          <Details>
-            <span>1 hour 14 mins</span>
-            <span>+16</span>
-            <span>1999</span>
-          </Details>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Praesentium hic rem eveniet error possimus, neque ex doloribus.
-          </p>
-          <h6>Action</h6>
-        </ItemInfo>
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <ItemInfo>
+            <Icons>
+              <BsPlayFill />
+              <MdOutlineAdd />
+              <AiOutlineLike />
+              <AiOutlineDislike />
+            </Icons>
+            <Details>
+              <span>1 hour 14 mins</span>
+              <span>+16</span>
+              <span>1999</span>
+            </Details>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+            </p>
+            <h6>Action</h6>
+          </ItemInfo>
+        </>
       )}
     </ItemContainer>
   )
