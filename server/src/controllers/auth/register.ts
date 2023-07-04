f5d1e1b10394313d6e7ca4dbb9666f4ff3be1d53
justtfelix <justttfelix@gmail.com>
2023-07-04 12:19:01 +0400

    [Server] Added register controller functionality

--- server/src/controllers/auth/register.ts
+++ server/src/controllers/auth/register.ts
@@ -1,8 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+import { User } from '../../models';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.send('Register');
+    const { username, email, password } = req.body;
+
+    const isUsername = await User.findOne({ username });
+    if (isUsername) {
+      return res.status(409).json({
+        status: 'fail',
+        message: 'Username already taken',
+      });
+    }
+
+    const isEmail = await User.findOne({ email });
+    if (isEmail) {
+      return res.status(409).json({
+        status: 'fail',
+        message: 'Email already taken',
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({ status: 'success', user });
   } catch (err) {
     next(err);
   }
