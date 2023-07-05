6af9a4dcc9b4bd9cf85ca74f63ec0bd1396ff67a
justtfelix <justttfelix@gmail.com>
2023-07-05 09:54:01 +0400

    [Server] Added login controller functionality

--- server/src/controllers/auth/login.ts
+++ server/src/controllers/auth/login.ts
@@ -1,8 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+import { User } from '../../models';
+import { Status } from '../../constants';
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.send('Login');
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        status: Status.FAIL,
+        message: 'Incorrect email or password',
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        status: Status.FAIL,
+        message: 'Incorrect email or password',
+      });
+    }
+
+    return res.status(200).json({ status: Status.SUCCESS, user });
   } catch (err) {
     next(err);
   }
